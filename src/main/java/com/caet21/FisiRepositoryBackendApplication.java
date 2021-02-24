da368766f8d2e67445b719088a63104701b8191f
francomartinezroncal <jean.martinez@unmsm.edu.pe>
2021-02-23 20:14:27 -0500

    Estructurado del Proyecto V0.0.1

---
+++ src/main/java/com/caet21/FisiRepositoryBackendApplication.java
@@ -0,0 +1,12 @@
+package com.caet21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisiRepositoryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisiRepositoryBackendApplication.class, args);
+	}
+}
